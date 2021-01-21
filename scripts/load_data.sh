5097e842d84cf03cc686c39e367ece6b952719ed
David Horsey <davehorsey@users.noreply.github.com>
2021-01-21 19:56:22 +0800

    Updated scripts to download from osm website

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -6,12 +6,20 @@ source ./scripts/load_data.cfg
 # db_user=my_username
 # db_password=my_secure_password
 
-ogr2ogr \
-    -f PostgreSQL PG:"host=localhost user=$db_user password=$db_password \
-    dbname=kal_mtb" ./data/osm-export.geojson \
-    -nln mtb_trails -lco GEOMETRY_NAME=geom
+# ogr2ogr \
+#     -f PostgreSQL PG:"host=localhost user=$db_user password=$db_password \
+#     dbname=kal_mtb" ./data/osm-export.geojson \
+#     -nln mtb_trails -lco GEOMETRY_NAME=geom
 
-ogr2ogr \
-    -f PostgreSQL PG:"host=localhost user=$db_user password=$db_password \
-    dbname=kal_mtb" ./data/kmtn-aoi.geojson \
-    -nln mtb_trails_aoi -lco GEOMETRY_NAME=geom
+# ogr2ogr \
+#     -f PostgreSQL PG:"host=localhost user=$db_user password=$db_password \
+#     dbname=kal_mtb" ./data/kmtn-aoi.geojson \
+#     -nln mtb_trails_aoi -lco GEOMETRY_NAME=geom
+
+osm2pgrouting \
+  --username $db_user \
+  --password $db_password \
+  --host 127.0.0.1 \
+  --dbname kal_mtb \
+  --file ./data/kalamunda.osm \
+  --conf ./scripts/mapconfig_for_bicycles.xml
---
+++ scripts/osm_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O kalamunda.osm "https://api.openstreetmap.org/api/0.6/map?bbox=116.09489731354228,-31.992841127551777,116.15877755109821,-31.935119256087205"
